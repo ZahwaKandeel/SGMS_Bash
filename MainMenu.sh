cab907d280b24ea0836cb7a8c71055b7bd89a3a2
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-04-15 21:28:54 +0200

    adding the main menu

--- MainMenu.sh
+++ MainMenu.sh
@@ -1,1 +1,30 @@
-test
+#! /usr/bin/bash
+
+source ./StudentManagement.sh
+
+
+echo Main Menu:
+
+declare -a options=("Manage Students" "Manage Subjects" "Manage Grades" "Reports & Statistics" "Exit")
+
+select option in "${options[@]}"
+do
+	case $option in 
+		"Manage Students")
+		
+			;;
+		"Manage Subjects")
+		
+			;;
+		"Manage Grades")
+			
+			;;
+		"Reports & Statistics")
+			
+			;;
+		"Exit")
+			break
+			;;
+esac
+
+done
