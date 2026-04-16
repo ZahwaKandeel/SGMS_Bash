0ab3746631ff30a4fe957d7da540176888b3508d
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-04-16 13:30:27 +0200

    update in grade management function calling in main menu

--- GradeManagement.sh
+++ GradeManagement.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 Grade_Management_Menu(){
-echo Student Management:
+echo Grade Management:
 
 declare -a options=("Assign Grade to Student" "Update Existing Grade" "Delete a Grade" "View Grades by Subject" "View Grades by Student" "Back to main menu")
 
