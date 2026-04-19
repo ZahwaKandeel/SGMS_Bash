a5ea63a3e26d464d76347a081ffe708f37cb0271
nada-aymann <na835126@gmail.com>
2026-04-19 15:20:11 +0200

    Added update_grade function

--- GradeManagement.sh
+++ GradeManagement.sh
@@ -12,7 +12,7 @@ do
 		Assign_Grade_to_Student
 			;;
 		"Update Existing Grade")
-			
+			update_grade
 			;;
 		"Delete a Grade")
 			
@@ -100,4 +100,77 @@ fi
 echo "$studId|$studScore|$letter" >> "$file"
 
 echo "Student grade assigned successfully"
+}
+
+update_grade(){
+while true
+do
+	read -p "Enter subject code : " subCode
+	if [[ -z $subCode || ! $subCode =~ ^[A-Z]{2,5}[0-9]{2,4}$ ]]
+	then
+	echo "Invalid subject code, it must be 2–5 uppercase letters + 2–4 digits"
+	continue
+	fi
+	if [[ ! -f sgms_data/grades/$subCode.grd ]]
+	then
+	echo "No grades for this subject"
+	continue
+	fi
+	break
+done
+cat sgms_data/grades/$subCode.grd
+
+while true
+do
+	read -p "Enter student id that you want to update his grade : " studID
+	if [[ -z $studID || ! "$studID" =~ ^[0-9]{1,10}$ ]]
+	then
+	echo "Invalid student id, it must be number"
+	continue
+	fi
+	if ! grep -q "^$studID|" sgms_data/grades/$subCode.grd
+	then
+	echo "This student doesn't exist"
+	continue
+	fi
+	break
+done
+
+while true
+do
+	read -p "Enter new score : " newScore
+	if [[ -z $newScore ]]
+	then
+	echo "You should enter a score"
+	continue
+	fi
+	if [[ ! $newScore =~ ^[0-9]+(\.[0-9]+)?$ ]]
+	then
+	echo "Invalid score format, it must be float number between 0.0 and 100.0"
+	continue
+	fi
+	if awk "BEGIN {exit !($newScore > 100 || $newScore < 0 )}"
+	then
+	echo "Score must be between 0.0 and 100.0"
+	continue
+	fi
+	break
+done
+
+if awk "BEGIN {exit !($newScore >= 90)}"; then newLetter="A+"
+elif awk "BEGIN {exit !($newScore >= 85)}"; then newLetter="A"
+elif awk "BEGIN {exit !($newScore >= 80)}"; then newLetter="A-"
+elif awk "BEGIN {exit !($newScore >= 75)}"; then newLetter="B+"
+elif awk "BEGIN {exit !($newScore >= 70)}"; then newLetter="B"
+elif awk "BEGIN {exit !($newScore >= 65)}"; then newLetter="B-"
+elif awk "BEGIN {exit !($newScore >= 60)}"; then newLetter="C+"
+elif awk "BEGIN {exit !($newScore >= 55)}"; then newLetter="C"
+elif awk "BEGIN {exit !($newScore >= 50)}"; then newLetter="C-"
+elif awk "BEGIN {exit !($newScore >= 45)}"; then newLetter="D"
+else newLetter="F"
+fi
+
+echo "Grade updated successfully"
+sed -i "s/^$studID|.*/$studID|$newScore|$newLetter/" sgms_data/grades/$subCode.grd
+grep "^$studID|" sgms_data/grades/$subCode.grd
 }
